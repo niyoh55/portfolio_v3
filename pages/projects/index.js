1d30f49088f5ba1c25a062b238ee8270bd3409af
niyoh55 <niyohedwyn21@gmail.com>
2022-07-13 17:09:14 +0800

    added a project, fixed responsiveness in Projects Page

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import EngineeringOutlinedIcon from "@mui/icons-material/EngineeringOutlined";
 const index = () => {
   return (
-    <div className="flex justify-center items-center w-screen font-nunito min-h-screen">
+    <div className="flex md:justify-center md:items-center w-screen font-nunito min-h-screen">
       <Head>
         <title>Projects</title>
       </Head>
@@ -14,10 +14,10 @@ const index = () => {
 
       <div className="flex flex-col w-full h-full pt-10">
         <div className="flex justify-center">
-          <p className="text-8xl text-white">Projects</p>
+          <p className="text-6xl md:text-8xl text-white">Projects</p>
         </div>
 
-        <div className="grid grid-cols-2 gap-4 px-10 mt-10 w-full">
+        <div className="grid grid-cols-1 xl:grid-cols-2 gap-4 px-2 md:px-10 mt-10 w-full">
           <div
             className="aspect-video min-w-[30%] bg-contain bg-no-repeat h-auto w-auto bg-center bg-gray-200 group shadow-2xl"
             style={{
@@ -25,34 +25,44 @@ const index = () => {
             }}
           >
             <div className="flex flex-col justify-center items-center h-full w-full bg-opacity-70 bg-black transition-all duration-300">
-              <p className="text-6xl text-white opacity-100 group-hover:opacity-0 group-hover:hidden transition-all duration-300">
+              <p className="text-xl md:text-6xl text-white text-center opacity-100 group-hover:opacity-0 group-hover:hidden transition-all duration-300">
                 Simple Social Media Website
               </p>
-              <p className="text-6xl text-white hidden opacity-0 group-hover:opacity-100 group-hover:flex transition-all duration-300 px-20">
-                Create your own posts. Like or comment on posts from other
-                users. Still adding new features.
+              <p className="texl-lg md:text-4xl text-white hidden opacity-0 group-hover:opacity-100 group-hover:flex transition-all duration-300 px-20">
+                {/* Create your own posts. Like or comment on posts from other
+                users. Still adding new features. */}
+                Made with NextJS + TailwindCSS and MongoDB + mongoose for
+                backend. Still under development.
               </p>
-              <button className="mt-5 py-2 px-5 hidden opacity-0 group-hover:opacity-100 group-hover:flex rounded-sm bg-white text-black ">Visit</button>
+              <a href="https://socmed-next.vercel.app/">
+                <button className="mt-5 py-2 px-5 hidden opacity-0 group-hover:opacity-100 group-hover:flex rounded-sm bg-white text-black ">
+                  Visit
+                </button>
+              </a>
             </div>
           </div>
-{/* 
           <div
             className="aspect-video min-w-[30%] bg-contain bg-no-repeat h-auto w-auto bg-center bg-gray-200 group shadow-2xl"
             style={{
-              backgroundImage: `url("https://i.ibb.co/k9H0GNS/socmed-next.png")`,
+              backgroundImage: `url("https://i.ibb.co/tMQktM7/1657532466504.png")`,
             }}
           >
             <div className="flex flex-col justify-center items-center h-full w-full bg-opacity-70 bg-black transition-all duration-300">
-              <p className="text-6xl text-white opacity-100 group-hover:opacity-0 group-hover:hidden transition-all duration-300">
-                Simple Social Media Website
+              <p className="text-xl md:text-6xl text-white text-center opacity-100 group-hover:opacity-0 group-hover:hidden transition-all duration-300">
+                3D Printing Shop(E-Commerce Sample)
               </p>
-              <p className="text-6xl text-white hidden opacity-0 group-hover:opacity-100 group-hover:flex transition-all duration-300 px-20">
-                Create your own posts. Like or comment on posts from other
-                users. Still adding new features.
+              <p className="texl-lg md:text-4xl text-white hidden opacity-0 group-hover:opacity-100 group-hover:flex transition-all duration-300 px-20">
+                {/* Create your own posts. Like or comment on posts from other
+                users. Still adding new features. */}
+                Made with NextJS + TailwindCSS. Still under development.
               </p>
+              <a href="https://we-printz.vercel.app/">
+                <button className="mt-5 py-2 px-5 hidden opacity-0 group-hover:opacity-100 group-hover:flex rounded-sm bg-white text-black ">
+                  Visit
+                </button>
+              </a>
             </div>
-          </div> */}
-
+          </div>
         </div>
       </div>
     </div>
@@ -60,3 +70,23 @@ const index = () => {
 };
 
 export default index;
+
+{
+  /* 
+          <div
+            className="aspect-video min-w-[30%] bg-contain bg-no-repeat h-auto w-auto bg-center bg-gray-200 group shadow-2xl"
+            style={{
+              backgroundImage: `url("https://i.ibb.co/k9H0GNS/socmed-next.png")`,
+            }}
+          >
+            <div className="flex flex-col justify-center items-center h-full w-full bg-opacity-70 bg-black transition-all duration-300">
+              <p className="text-6xl text-white opacity-100 group-hover:opacity-0 group-hover:hidden transition-all duration-300">
+                Simple Social Media Website
+              </p>
+              <p className="text-6xl text-white hidden opacity-0 group-hover:opacity-100 group-hover:flex transition-all duration-300 px-20">
+                Create your own posts. Like or comment on posts from other
+                users. Still adding new features.
+              </p>
+            </div>
+          </div> */
+}
